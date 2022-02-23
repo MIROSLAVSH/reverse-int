8f5f32410ecad3e4b721f10bc9784f576381f76e
MIROSLAVSH <sharkovskiymiroslav@gmail.com>
2022-02-23 17:23:12 +0300

    gh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function reverse (n) {
+// module.exports = function reverse (n) {
   
+// }
+
+// const reverseNum = n => parseFloat(n.toString().split('').reverse().join('')) * Math.sign(n)
+// module.exports = function reverseNum(n) {
+//     return (
+//         parseFloat(
+//             n
+//                 .toString()
+//                 .split('')
+//                 .reverse()
+//                 .join('')
+//         ) * Math.sign(n)
+//     )
+// }
+// return reverseNum()
+
+const reversedNum = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num)
+module.exports = function reversedNum(num) {
+    return (
+        parseFloat(
+            num
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+        ) * Math.sign(num)
+    )
+    
 }
+return reversedNum
